0953a80602daa4ea6be75c8a45bfe9921a7cad94
Ignas Šimaitis <ignas.simaitis06@gmail.com>
2026-05-28 21:42:39 +0300

    Declare custom Vector student functions

--- Objektinis/VectorFunkcijos.h
+++ Objektinis/VectorFunkcijos.h
@@ -40,3 +40,38 @@ void atliktiDuomenuApdorojimoTyrimoVidurkiVector(
 	int rusiavimoTipas,
 	int kartu
 );
+
+void skaitytiIsFailoManoVector(const std::string& failoPavadinimas, Vector<Studentas>& studentai);
+void skaiciuotiRezultatusManoVector(Vector<Studentas>& studentai, int skaiciavimoTipas);
+void rusiuotiStudentusManoVector(Vector<Studentas>& studentai, int pasirinkimas);
+void isvestiRezultatusManoVector(const Vector<Studentas>& studentai, int skaiciavimoTipas);
+void isvestiRezultatusIFailaManoVector(const Vector<Studentas>& studentai, int skaiciavimoTipas, const std::string& failoPavadinimas);
+
+void padalintiStudentus1ManoVector(
+	const Vector<Studentas>& studentai,
+	Vector<Studentas>& nuskriaustieji,
+	Vector<Studentas>& kietiakiai
+);
+
+void padalintiStudentus2ManoVector(
+	Vector<Studentas>& studentai,
+	Vector<Studentas>& nuskriaustieji
+);
+
+void padalintiStudentus3ManoVector(
+	Vector<Studentas>& studentai,
+	Vector<Studentas>& nuskriaustieji
+);
+
+void atliktiDuomenuApdorojimoTyrimaManoVector(
+	const std::string& failoPavadinimas,
+	int skaiciavimoTipas,
+	int rusiavimoTipas
+);
+
+void atliktiDuomenuApdorojimoTyrimoVidurkiManoVector(
+	const std::string& failoPavadinimas,
+	int skaiciavimoTipas,
+	int rusiavimoTipas,
+	int kartu
+);
